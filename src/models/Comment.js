90bbb03429df4c48f21640471ffe8fd467f285ad
pr3n3y <pranay.ambre@torero.in>
2024-08-09 18:56:34 +0530

    UPDTAE

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,30 +1,31 @@
 // models/Post.js
 
 import mongoose from "mongoose";
-import { Reply } from "./Reply";
 
-
-const CommentSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
+const CommentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    caption: {
+      type: String,
+      required: true,
+    },
+    commentedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    replies: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Reply",
+      },
+    ],
   },
-  caption: {
-    type: String,
-    required: true,
-  },
-  commentedAt: {
-    type: Date,
-    default: Date.now,
-  },
-  replies: [Reply],
-},
-{ timestamps: true });
+  { timestamps: true }
+);
 
-
-
-
-
-
-export const Comment = mongoose.models.Comment || mongoose.model("Comment", CommentSchema);
+export const Comment =
+  mongoose.models.Comment || mongoose.model("Comment", CommentSchema);
--- src/models/Post.js
+++ src/models/Post.js
@@ -4,8 +4,6 @@ import mongoose from "mongoose";
 import { Comment } from "./Comment";
 import { Like } from "./Like";
 
-
-
 const PostSchema = new mongoose.Schema(
   {
     username: {
@@ -14,13 +12,13 @@ const PostSchema = new mongoose.Schema(
     },
     caption: {
       type: String,
-      required: function() {
+      required: function () {
         return !this.images || this.images.length === 0;
       },
     },
     images: {
       type: [String],
-      required: function() {
+      required: function () {
         return !this.caption;
       },
     },
@@ -29,13 +27,20 @@ const PostSchema = new mongoose.Schema(
       match: /^#[0-9A-F]{6}$/i, // Ensures it is a valid hex color code
       required: true,
     },
-    likes: [Like],
-    comments: [Comment],
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Like",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
   { timestamps: true }
 );
-
-
-
 
 export const Post = mongoose.models.Post || mongoose.model("Post", PostSchema);
